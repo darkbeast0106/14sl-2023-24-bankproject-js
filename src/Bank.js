d7e9c8ef614ef980ade508056f641d84dd365647
Budaházi Bence <budahazi.bence@petrik.hu>
2024-02-05 20:46:34 +0100

    docs: bank osztály dokumentálásának kiegészítése

--- src/Bank.js
+++ src/Bank.js
@@ -1,3 +1,7 @@
+// ECMAscript modul esetén export class Bank
+/**
+ * Bank műveleteit végrehajtó osztály.
+ */
 class Bank {
     /**
      * Új számlát nyit a megadott névvel, számlaszámmal, 0 Ft egyenleggel
@@ -9,32 +13,33 @@ class Bank {
     }
 
     /**
-     * 
-     * @param {string} szamlaszam 
-     * @returns {number}
+     * Lekérdezi az adott számlán lévő pénzösszeget
+     * @param {string} szamlaszam A számla számlaszáma, aminek az egyenlegét keressük
+     * @returns {number} A számlán lévő egyenleg
      */
     egyenleg(szamlaszam) {
         
     }
     
     /**
-     * 
-     * @param {string} szamlaszam 
-     * @param {number} osszeg 
+     * Egy létező számlára pénzt helyez
+     * @param {string} szamlaszam A számla számlaszáma, amire pénzt helyez
+     * @param {number} osszeg A számlára helyezendő pénzösszeg
      */
     egyenlegFeltolt(szamlaszam, osszeg) {
 
     }
 
     /**
-     * 
-     * @param {string} honnan 
-     * @param {string} hova 
-     * @param {number} osszeg 
-     * @returns {boolean}
+     * Két számla között utal.
+     * Ha nincs elég pénz a forrás számlán, akkor false értékkel tér vissza, és nem történik utalás
+     * @param {string} honnan A forrás számla számlaszáma
+     * @param {string} hova >A cél számla számlaszáma
+     * @param {number} osszeg Az átutalandó egyenleg
+     * @returns {boolean} Az utalás sikeressége
      */
     utal(honnan, hova, osszeg) {
-        
+
     }
 }
 
--- src/Bank.test.js
+++ src/Bank.test.js
@@ -1,3 +1,7 @@
+/*
+ECMAscript modul esetén (JEST-nél csak további könyvtárakkal működik):
+ import { Bank } from "./Bank";
+*/
 const { Bank } = require('./Bank');
 
 test('ujSzamla - Létrehozott számla egyenlege 0', () => {
